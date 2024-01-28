1c1dce7fdf8049e7f27460343d04bf0ccc3565ad
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2024-01-28 19:24:08 +0100

    adjust nexj.js project and configure i18next

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en", "pl"],
+    defaultLocale: "pl",
+    localeDetection: false
+  }
 };
 
 export default nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,24 @@
 import "@/styles/globals.css";
+import { useRouter } from "next/router";
+import { IntlProvider } from "react-intl";
+
+import en from '../i18n/en.json';
+import pl from '../i18n/pl.json';
+
+const messages = {
+  en,
+  pl
+}
+
+function getDirection(locale) {
+  return "ltr"
+}
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const { locale } = useRouter();
+  return (
+    <IntlProvider locale={locale} messages={messages[locale]}>
+      <Component {...pageProps} dir={getDirection(locale)} />
+    </IntlProvider>
+  )
 }
