145de3c8028d66590123ae5057e8c700b4f830ab
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2025-02-12 23:17:03 +0100

    fix pizza list

--- src/components/FourSection/FourSection.tsx
+++ src/components/FourSection/FourSection.tsx
@@ -10,12 +10,12 @@ const FourSection = () => {
   const { tString } = useTranslation(namespaces.common);
 
   const pizzas = [
-    { src: "/images/pizza1.png", name: tString("fourSection.pizza1.name"), description: tString("fourSection.pizza1.description") },
-    { src: "/images/pizza2.png", name: tString("fourSection.pizza2.name"), description: tString("fourSection.pizza2.description") },
-    { src: "/images/pizza3.png", name: tString("fourSection.pizza3.name"), description: tString("fourSection.pizza3.description") },
-    { src: "/images/pizza4.png", name: tString("fourSection.pizza4.name"), description: tString("fourSection.pizza4.description") },
-    { src: "/images/pizza5.png", name: tString("fourSection.pizza5.name"), description: tString("fourSection.pizza5.description") },
-    { src: "/images/pizza6.png", name: tString("fourSection.pizza6.name"), description: tString("fourSection.pizza6.description") },
+    { src: "/images/pizza1.png", name: tString("fourSection.pizza1.name"), description: tString("fourSection.pizza1.description"), number: 1 },
+    { src: "/images/pizza2.png", name: tString("fourSection.pizza2.name"), description: tString("fourSection.pizza2.description"), number: 2 },
+    { src: "/images/pizza3.png", name: tString("fourSection.pizza3.name"), description: tString("fourSection.pizza3.description"), number: 3 },
+    { src: "/images/pizza6.png", name: tString("fourSection.pizza6.name"), description: tString("fourSection.pizza6.description"), number: 6 },
+    { src: "/images/pizza5.png", name: tString("fourSection.pizza5.name"), description: tString("fourSection.pizza5.description"), number: 5 },
+    { src: "/images/pizza4.png", name: tString("fourSection.pizza4.name"), description: tString("fourSection.pizza4.description"), number: 4 },
   ];
 
   return (
@@ -31,9 +31,9 @@ const FourSection = () => {
             />
             <div className="absolute w-full h-full flex justify-center items-center">
               {pizzas.map((pizza, index) => (
-                <div key={index} className={`absolute bg-white px-3 py-1 rounded-md shadow-md transition-transform transform hover:rotate-3 hover:scale-105 xl:w-[300px] w-[180px] ${index === 0 ? 'xl:top-12 xl:left-[-14rem] md:top-[-1rem] md:left-[-10rem]' : index === 1 ? 'xl:top-[-1rem] xl:right-[-12rem] md:top-[-2.5rem] md:right-[-10rem]' : index === 2 ? 'xl:top-[40%] xl:right-[-14rem] md:top-[50%] md:right-[-12rem] transform -translate-y-1/2' : index === 3 ? 'xl:top-1/2 xl:left-[-14rem] md:top-[60%] md:left-[-12rem] transform -translate-y-1/2' : index === 4 ? 'xl:bottom-[-.5rem] xl:left-[-13rem] md:bottom-[-2rem] md:left-[-10rem]' : 'xl:bottom-0 xl:right-[-12rem] md:bottom-[-1rem] md:right-[-10rem]'}`}>
+                <div key={index} className={`absolute bg-white px-3 py-1 rounded-md shadow-md transition-transform transform hover:rotate-3 hover:scale-105 xl:w-[300px] w-[180px] ${index === 0 ? 'xl:top-12 xl:left-[-14rem] md:top-[-3rem] md:left-[-10rem]' : index === 1 ? 'xl:top-[-1rem] xl:right-[-12rem] md:top-[-2.5rem] md:right-[-10rem]' : index === 2 ? 'xl:top-[40%] xl:right-[-14rem] md:top-[50%] md:right-[-12rem] transform -translate-y-1/2' : index === 3 ? 'xl:top-[48%] xl:left-[-14rem] md:top-[60%] md:left-[-12rem] transform -translate-y-1/2' : index === 4 ? 'xl:bottom-[-1rem] xl:left-[-13rem] md:bottom-[-4rem] md:left-[-10rem]' : 'xl:bottom-1 xl:right-[-14rem] md:bottom-[-1rem] md:right-[-10rem]'}`}>
                   <p className="text-md font-bold text-primary md:text-[12px] xl:text-base">
-                    {index + 1}️ {pizza.name}
+                    {pizza.number}️ {pizza.name}
                   </p>
                   <p className="text-xs md:text-[10px] xl:text-sm">
                     {pizza.description}
@@ -46,7 +46,7 @@ const FourSection = () => {
             {pizzas.map((pizza, index) => (
               <div key={index} className="flex flex-col items-center gap-2 hover:scale-105 transition-transform">
                 <Image src={pizza.src} alt={pizza.name} width={200} height={100} className="rounded-lg" />
-                <p className="text-md font-bold text-primary mt-2">{pizza.name}</p>
+                <p className="text-md font-bold text-primary mt-2">{pizza.number} {pizza.name}</p>
                 <p className="text-xs text-textDark">{pizza.description}</p>
               </div>
             ))}
