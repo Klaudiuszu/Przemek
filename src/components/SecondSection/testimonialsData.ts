3f93991bf053004f971d3ee05b7932cab1ac20cd
Klaudiusz Pupiec <klaudiuszpupiec.kontakt@gmail.com>
2025-03-26 18:44:57 +0100

    testimonials

--- src/components/SecondSection/testimonialsData.ts
+++ src/components/SecondSection/testimonialsData.ts
@@ -5,11 +5,41 @@ interface Testimonial {
   textKey: string;
 }
 
-export const testimonialsData: Testimonial[] = [
+export const testimonialsData:Testimonial[] = [
   {
     id: 1,
-    name: '',
-    date: '',
-    textKey: "",
+    name: "Anna i Marek",
+    date: "Wesele, 2023",
+    textKey: "testimonials.opinion1"
   },
+  {
+    id: 2,
+    name: "Krzysztof",
+    date: "Kawalerski, 2024",
+    textKey: "testimonials.opinion2"
+  },
+  {
+    id: 3,
+    name: "Magda",
+    date: "18-stka, 2023",
+    textKey: "testimonials.opinion3"
+  },
+  {
+    id: 4,
+    name: "Tomek",
+    date: "18-stka, 2024",
+    textKey: "testimonials.opinion4"
+  },
+  {
+    id: 5,
+    name: "Kasia i Piotr",
+    date: "Wesele, 2024",
+    textKey: "testimonials.opinion5"
+  },
+  {
+    id: 6,
+    name: "Michał",
+    date: "Kawalerski, 2023",
+    textKey: "testimonials.opinion6"
+  }
 ];
